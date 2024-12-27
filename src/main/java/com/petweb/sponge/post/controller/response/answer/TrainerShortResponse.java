a2fd4af2413ebb1ca7ee44589179d700c23117d6
mungdori <mungdori999@gmail.com>
2024-12-27 16:03:54 +0900

    훈련사 답변 response수정

--- src/main/java/com/petweb/sponge/post/controller/response/answer/TrainerShortResponse.java
+++ src/main/java/com/petweb/sponge/post/controller/response/answer/TrainerShortResponse.java
@@ -10,6 +10,7 @@ public class TrainerShortResponse {
 
     private Long id;
     private String name; //이름
+    private String profileImgUrl; //프로필 이미지 링크
     private int adoptCount; // 채택 답변 수
     private int chatCount; // 1대1 채팅 수
 
@@ -17,6 +18,7 @@ public class TrainerShortResponse {
         return TrainerShortResponse.builder()
                 .id(trainer.getId())
                 .name(trainer.getName())
+                .profileImgUrl(trainer.getProfileImgUrl())
                 .adoptCount(trainer.getAdoptCount())
                 .chatCount(trainer.getChatCount())
                 .build();
