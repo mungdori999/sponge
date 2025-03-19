1126ca23858cf57df3e60294805deb2eae6ddca1
mungdori <mungdori999@gmail.com>
2025-03-19 10:08:27 +0900

    훈련사 리뷰 repository 테스트 추가

--- src/main/java/com/petweb/sponge/trainer/repository/ReviewJpaRepository.java
+++ src/main/java/com/petweb/sponge/trainer/repository/ReviewJpaRepository.java
@@ -7,8 +7,9 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.Optional;
 
-public interface ReviewJpaRepository extends JpaRepository<ReviewEntity, Long>, ReviewQueryDslRepository {
+public interface ReviewJpaRepository extends JpaRepository<ReviewEntity, Long> {
     @Query("SELECT r FROM ReviewEntity r WHERE r.userId = :loginId AND r.trainerId=:trainerId")
     Optional<ReviewEntity> findByUserId(@Param("loginId") Long loginId, @Param("trainerId") Long trainerId);
-
+    @Query(value = "SELECT * FROM Review r WHERE r.trainer_id = :trainerId ORDER BY r.created_at DESC LIMIT :limit OFFSET :offset", nativeQuery = true)
+    List<ReviewEntity> findListByTrainerId(@Param("trainerId") Long trainerId, @Param("limit") int limit, @Param("offset") int offset);
 }
--- src/main/java/com/petweb/sponge/trainer/repository/ReviewRepositoryImpl.java
+++ src/main/java/com/petweb/sponge/trainer/repository/ReviewRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.petweb.sponge.trainer.repository;
 
 import com.petweb.sponge.trainer.domain.Review;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,6 +13,7 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class ReviewRepositoryImpl implements ReviewRepository {
     private final ReviewJpaRepository reviewJpaRepository;
+    private static final int PAGE_SIZE = 10;
 
     @Override
     public Optional<Review> findByUserId(Long loginId, Long trainerId) {
@@ -20,7 +22,8 @@ public class ReviewRepositoryImpl implements ReviewRepository {
 
     @Override
     public List<Review> findListByTrainerId(Long trainerId, int page) {
-        return reviewJpaRepository.findByTrainerId(trainerId,page).stream().map(ReviewEntity::toModel).collect(Collectors.toList());
+        int offset = page * PAGE_SIZE;
+        return reviewJpaRepository.findListByTrainerId(trainerId, PAGE_SIZE,offset).stream().map(ReviewEntity::toModel).collect(Collectors.toList());
     }
 
     @Override
